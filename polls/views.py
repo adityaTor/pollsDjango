ddc2ca002978f8c180b98fdc1288a9e9ec47ceb8
Aditya Tornala <adityatrnla@outlook.com>
2017-02-13 00:58:05 -0500

    added poll creation

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import ListView, DetailView, UpdateView, CreateView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, View
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib import auth
+from django.utils import timezone
 from .models import Question, Choice
-from polls.forms import QuestionForm, ChoiceForm
+from polls.forms import QuestionForm, ChoiceForm, registrationForm
 from django.forms import formset_factory, inlineformset_factory, modelformset_factory
 # Create your views here.
 
@@ -26,8 +29,38 @@ def vote(request, question_id):
 	return HttpResponse(selected_choice.votes)
 
 def test(request):
-	form = formset_factory(ChoiceForm, extra=2)
-	return render(request, 'polls/create_poll.html', {'form': form})
+	pass
+
+
+class registration(View):
+	form_class = registrationForm
+	template_name = 'polls/registration.html'
+
+	def get(self,request):
+		form = self.form_class(None)
+		return render(request,self.template_name,{'form':form})
+
+	def post(self,request):
+		form = self.form_class(request.POST)
+
+		if form.is_valid():
+
+			user = form.save(commit=False)
+
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			email = form.cleaned_data['email']
+			user.set_password(password)
+			user.save()
+
+			user = authenticate(username=username,email=email,password=password)
+
+			if user is not None:
+				if user.is_active:
+					auth.login(request,user)
+					return redirect('polls:index')
+
+		return render(request,self.template_name,{'form':form})
 
 
 
@@ -41,7 +74,31 @@ class userPolls(ListView, LoginRequiredMixin):
 
 @login_required
 def createPoll(request):
-	pass
+
+	pollFormSet = formset_factory(ChoiceForm, extra = 2, max_num = 6)
+	if request.method == 'POST':
+		form = QuestionForm(request.POST)
+		formset = pollFormSet(request.POST, request.FILES)
+		if form.is_valid() and formset.is_valid():
+			question = form.save(commit=False)
+			question.poster = request.user
+			question.pub_date = timezone.now()
+			question.save()
+
+			for form in formset:
+				choice = Choice(choice=form.save(commit=False))
+				choice.question = Question.objects.get(pk=question.id)
+				choice.save()
+
+
+		return redirect('polls:index')
+	else:
+		form = QuestionForm()
+		formset = pollFormSet()
+
+	return render(request, 'polls/create_poll.html', {'questionForm':form, 'choiceFormset':formset})
+
+
 
 
 	
